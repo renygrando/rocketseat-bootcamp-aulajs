30826bb8944b3bfaabe7a05477e70a0854e49628
Reny Grando <renygrando@gmail.com>
2020-04-08 00:47:08 -0300

    envio inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+//Criar um Programa que calcula a média
+// das notas entre os alunos e envia
+//mensagem do cálculo da média
+//Se a média for maior que 5, parabenizar a turma!
+
+const aluno01 = 'Reny Grando'
+const notaAluno01 = 9.1
+
+const aluno02 = 'Valberto'
+const notaAluno02 = 10
+
+const aluno03 = 'Fulano'
+const notaAluno03 = 2.0
+
+const media = (notaAluno01+notaAluno02+notaAluno03) / 3
+
+console.log(media)
