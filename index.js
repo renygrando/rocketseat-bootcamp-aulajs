560f55b7cf241af2fc1d8e9b029d76885be1d059
Reny Grando <renygrando@gmail.com>
2020-04-08 21:59:16 -0300

    Implementando funcções

--- index.js
+++ index.js
@@ -3,15 +3,42 @@
 //mensagem do cálculo da média
 //Se a média for maior que 5, parabenizar a turma!
 
-const aluno01 = 'Reny Grando'
-const notaAluno01 = 9.1
+const alunosTurmaA = [
+    { nome: 'Joao', nota: 10 },
+    { nome: 'Maria', nota: 9.8 },
+    { nome: 'Keila', nota: 8.9 },
 
-const aluno02 = 'Valberto'
-const notaAluno02 = 10
+]
 
-const aluno03 = 'Fulano'
-const notaAluno03 = 2.0
+const alunosTurmaB = [
+    { nome: 'Joana', nota: 3.3 },
+    { nome: 'Carla', nota: 9.0 },
+    { nome: 'Lucas', nota: 6.1 },
+    { nome: 'Luis', nota: 10 },
+    { nome: 'Gean', nota: 10 },
+]
 
-const media = (notaAluno01+notaAluno02+notaAluno03) / 3
+function calculaMedia (alunos){
+    let soma = 0;
+    for (let i = 0; i < alunos.length; i++){
+        soma = soma + alunos[i].nota
+    }
+    
+    const media = soma / alunos.length
+    return media
+}
 
-console.log(media)
+const media1 = calculaMedia (alunosTurmaA);
+const media2 = calculaMedia (alunosTurmaB);
+
+function enviaMensagem(media, turma){
+if (media >= 5){
+    console.log (`Parabéns!!! A média da turma ${turma}  foi de ${media}.`)
+} else {
+    console.log (`Que pena! A média da turma ${turma} foi menor que 5 =(`)
+}
+}
+
+enviaMensagem(media1, 'A');
+enviaMensagem(media2, 'B');
+
