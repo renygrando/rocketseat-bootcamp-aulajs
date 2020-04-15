f143a5527521e5ac0f977f1f440a8eb9cd1d9c52
Reny Grando <renygrando@gmail.com>
2020-04-14 21:28:44 -0300

    marcar aluno como reprovado em index.js

--- usuariotecnologia.js
+++ usuariotecnologia.js
@@ -4,11 +4,29 @@ const usuarios = [
     { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
   ];
 
-  function imprimeTela (usuarios){
-    let imprime = 0;
-    for (let i = 0; i < usuarios.length; i++){
-        console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`);
-    } imprime++;
-    }
+//   function imprimeTela (usuarios){
+//     for (let i = 0; i < usuarios.length; i++){
+//         console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`);
+//     } 
+//     }
 
-imprimeTela(usuarios);
+// imprimeTela(usuarios);
+// Percorra o array de tecnologias do usuário até encontrar se ele trabalha com CSS
+// SE encontrar, retorne true da função, caso contrário retorne false
+
+
+function checaSeUsuarioUsaCSS(usuario) {
+  for (let tecnologia of usuario.tecnologias) {
+      if (tecnologia == 'CSS') return true
+  }
+
+  return false
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+  const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i])
+
+  if(usuarioTrabalhaComCSS) {
+      console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
+  }
+}
