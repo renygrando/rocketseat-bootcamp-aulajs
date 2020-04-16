7783771200e8faea2cd397572cfec3110a7d5b51
Reny Grando <renygrando@gmail.com>
2020-04-16 00:39:20 -0300

    testando

---
+++ javascript/aposentadoria.js
@@ -0,0 +1,15 @@
+const nome = 'Valberto Silvio Grando'
+const sexo = 'M'
+const idade = 65
+const contribuicao = 35
+
+const tempoContribuicao = idade + contribuicao
+
+const aposentadoriaHomem = sexo == 'M' && contribuicao >= 35 && tempoContribuicao >= 95
+const aposentadoriaMulher = sexo == 'F' && contribuicao >= 30 && tempocontribuicao >=85
+
+if (aposentadoriaHomem || aposentadoriaMulher) {
+  console.log(`${nome}, você pode se aposentar!`)
+} else {
+  console.log(`${nome}, você não pode se aposentar!`)
+}
